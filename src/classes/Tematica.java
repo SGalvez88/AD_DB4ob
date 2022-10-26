7351c303e354a3160fe46de124ce7ba2bbee97c2
sgalvez88 <sergio.galvez.reguera@iestubalcain.net>
2022-10-26 11:51:18 +0200

    Commit: commit inicial

---
+++ src/classes/Tematica.java
@@ -0,0 +1,14 @@
+
+package classes;
+
+import java.time.LocalDateTime;
+
+
+public class Tematica {
+    
+    String identificador;
+    String nombre;
+    LocalDateTime fechaModificacion;
+    LocalDateTime fechaAlta;
+    
+}
---
+++ src/controllers/ActividadEva2.java
@@ -0,0 +1,10 @@
+package controllers;
+
+
+public class ActividadEva2 {
+
+    public static void main(String[] args) {
+       
+    }
+    
+}
