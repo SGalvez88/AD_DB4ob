57902b3fca3301d5259699d423fa5285e4e8503e
SGalvez88 <sergio.galvez.reguera@iestubalcain.net>
2022-10-28 19:17:02 +0200

    commit: creación de clases

--- src/classes/Libro.java
+++ src/classes/Libro.java
@@ -1,6 +1,15 @@
 
 package classes;
 
+import java.time.LocalDateTime;
+
 public class Libro {
+    
+    String identificador;
+    String nombre;
+    String autor;
+    LocalDateTime yearPublicacion;
+    String editorial;
+    
     
 }
--- src/classes/Tematica.java
+++ src/classes/Tematica.java
@@ -10,5 +10,5 @@ public class Tematica {
     String nombre;
     LocalDateTime fechaModificacion;
     LocalDateTime fechaAlta;
-    
+       
 }
